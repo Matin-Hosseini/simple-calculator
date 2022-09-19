3571fe733ccf67c7e7f8e416ab66c2e59a0f2d20
Gymso <matinpinterest2003@gmail.com>
2022-09-19 20:28:33 +0430

    Dark Mode button UI created

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,14 +1,15 @@
 let body = document.querySelector('body');
 let darkBtn = document.querySelector("#dark-mode-btn");
 var clickCount = 0;
+let darkText = document.querySelector("#btn-text");
 darkBtn.addEventListener("click", ()=>{
-    
     clickCount++;
     body.classList.add("dark-mode");
-    darkBtn.textContent = "Light Mode";
+    darkText.textContent = "Light Mode:";
     if(clickCount==2){
         body.classList.remove("dark-mode");
+        darkText.textContent = "Dark Mode:";
         clickCount-=2;
-        darkBtn.textContent = "Dark Mode";
+        
     }
 })
